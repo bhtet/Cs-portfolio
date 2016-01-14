749f6abb75b6502264d4f173b25bf9748b0dac20
bhtet <bhtet.sh@mysummitps.org>
2016-01-14 05:30:37 +0000

    update

--- dice.js
+++ dice.js
@@ -2,6 +2,8 @@ var mycanvas = document.getElementById("mycanvas");
 var ctx = mycanvas.getContext("2d");
 
 var diceValues = [1, 2, 3, 4, 5, 6];
+var diceValues8Sided = [1, 2, 3, 4, 5, 6, 7, 8];
+var diceValues12Sided = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var button = document.getElementById("rollbutton");
 var result1 = document.getElementById("result1");
 var result2 = document.getElementById("result2");
@@ -12,12 +14,16 @@ var answer2;
 var answer3;
 var answer4;
 var dropDown = document.getElementById("dropdownbox");
+var diceType = document.getElementById("dropdowndicetype");
+var sumDropDown = document.getElementById("dropdownsum");
+var sumanswer = document.getElementById("sumspan");
+var uSmot = document.getElementById("smotimg");
 
 button.addEventListener("click", rollDie);
 
 function rollDie(){
     
-    if (dropDown.value == "1"){
+    if (dropDown.value == "1" && diceType.value == "six"){
     answer1= diceValues[Math.floor(Math.random()*diceValues.length)];
     answer2= "";
     answer3= "";
@@ -28,7 +34,7 @@ function rollDie(){
     result4.innerHTML = answer4;
     }
     
-    if (dropDown.value == "2"){
+    if (dropDown.value == "2" && diceType.value == "six"){
        answer1 = diceValues[Math.floor(Math.random()*diceValues.length)];
        answer2 = diceValues[Math.floor(Math.random()*diceValues.length)];
        answer3= "";
@@ -39,7 +45,7 @@ function rollDie(){
        result4.innerHTML = answer4;
     }
     
-    if (dropDown.value == "3"){
+    if (dropDown.value == "3" && diceType.value == "six"){
         answer1= diceValues[Math.floor(Math.random()*diceValues.length)];
         answer2= diceValues[Math.floor(Math.random()*diceValues.length)];
         answer3= diceValues[Math.floor(Math.random()*diceValues.length)];
@@ -50,7 +56,7 @@ function rollDie(){
         result4.innerHTML = answer4;
     }
     
-    if (dropDown.value == "4"){
+    if (dropDown.value == "4" && diceType.value == "six"){
         answer1= diceValues[Math.floor(Math.random()*diceValues.length)];
         answer2= diceValues[Math.floor(Math.random()*diceValues.length)];
         answer3= diceValues[Math.floor(Math.random()*diceValues.length)];
@@ -61,4 +67,95 @@ function rollDie(){
         result4.innerHTML = answer4;
     }
     
+    if (dropDown.value == "1" && diceType.value == "eight"){
+        answer1= diceValues8Sided[Math.floor(Math.random()*diceValues8Sided.length)];
+        answer2= "";
+        answer3= "";
+        answer4= "";
+        result1.innerHTML = answer1;
+        result2.innerHTML = answer2;
+        result3.innerHTML = answer3;
+        result4.innerHTML = answer4;
+    }
+    
+    if (dropDown.value == "2" && diceType.value == "eight"){
+        answer1= diceValues8Sided[Math.floor(Math.random()*diceValues8Sided.length)];
+        answer2= diceValues8Sided[Math.floor(Math.random()*diceValues8Sided.length)];
+        answer3= "";
+        answer4= "";
+        result1.innerHTML = answer1;
+        result2.innerHTML = answer2;
+        result3.innerHTML = answer3;
+        result4.innerHTML = answer4;
+    }
+    
+    if (dropDown.value == "3" && diceType.value == "eight"){
+        answer1= diceValues8Sided[Math.floor(Math.random()*diceValues8Sided.length)];
+        answer2= diceValues8Sided[Math.floor(Math.random()*diceValues8Sided.length)];
+        answer3= diceValues8Sided[Math.floor(Math.random()*diceValues8Sided.length)];
+        answer4= "";
+        result1.innerHTML = answer1;
+        result2.innerHTML = answer2;
+        result3.innerHTML = answer3;
+        result4.innerHTML = answer4;
+    }
+    
+    if (dropDown.value == "4" && diceType.value == "eight"){
+        answer1= diceValues8Sided[Math.floor(Math.random()*diceValues8Sided.length)];
+        answer2= diceValues8Sided[Math.floor(Math.random()*diceValues8Sided.length)];
+        answer3= diceValues8Sided[Math.floor(Math.random()*diceValues8Sided.length)];
+        answer4= diceValues8Sided[Math.floor(Math.random()*diceValues8Sided.length)];
+        result1.innerHTML = answer1;
+        result2.innerHTML = answer2;
+        result3.innerHTML = answer3;
+        result4.innerHTML = answer4;
+    }
+    
+    if (dropDown.value == "1" && diceType.value == "twelve"){
+        answer1= diceValues12Sided[Math.floor(Math.random()*diceValues12Sided.length)];
+        answer2= "";
+        answer3= "";
+        answer4= "";
+        result1.innerHTML = answer1;
+        result2.innerHTML = answer2;
+        result3.innerHTML = answer3;
+        result4.innerHTML = answer4;
+    }
+    
+    if (dropDown.value == "2" && diceType.value == "twelve"){
+        answer1= diceValues12Sided[Math.floor(Math.random()*diceValues12Sided.length)];
+        answer2= diceValues12Sided[Math.floor(Math.random()*diceValues12Sided.length)];
+        answer3= "";
+        answer4= "";
+        result1.innerHTML = answer1;
+        result2.innerHTML = answer2;
+        result3.innerHTML = answer3;
+        result4.innerHTML = answer4;
+    }
+    
+    if (dropDown.value == "3" && diceType.value == "twelve"){
+        answer1= diceValues12Sided[Math.floor(Math.random()*diceValues12Sided.length)];
+        answer2= diceValues12Sided[Math.floor(Math.random()*diceValues12Sided.length)];
+        answer3= diceValues12Sided[Math.floor(Math.random()*diceValues12Sided.length)];
+        answer4= "";
+        result1.innerHTML = answer1;
+        result2.innerHTML = answer2;
+        result3.innerHTML = answer3;
+        result4.innerHTML = answer4;
+    }
+    
+    if (dropDown.value == "4" && diceType.value == "twelve"){
+        answer1= diceValues12Sided[Math.floor(Math.random()*diceValues12Sided.length)];
+        answer2= diceValues12Sided[Math.floor(Math.random()*diceValues12Sided.length)];
+        answer3= diceValues12Sided[Math.floor(Math.random()*diceValues12Sided.length)];
+        answer4= diceValues12Sided[Math.floor(Math.random()*diceValues12Sided.length)];
+        result1.innerHTML = answer1;
+        result2.innerHTML = answer2;
+        result3.innerHTML = answer3;
+        result4.innerHTML = answer4;
+    }
+    
+    if (sumDropDown.value == "yes"){
+        sumanswer.innerHTML = "Sum:" + " " + (answer1 + answer2 + answer3 +answer4);
+    }
 }
