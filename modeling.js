409e9f99e2c6ea9dc7fadb641ce0b9d2635fc08f
bhtet <bhtet.sh@mysummitps.org>
2016-03-28 22:23:50 +0000

    update

--- modeling.js
+++ modeling.js
@@ -1,1 +1,22 @@
 // JavaScript File
+var mycanvas = document.getElementById("mycanvas");
+var ctx = mycanvas.getContext("2d");
+
+var firstText = document.getElementById("first_text")
+var nextButton = document.getElementById("nextbuttton")
+var backButton = document.getElementById("backbutton")
+var introHeader = document.getElementById("intro_header")
+var introText = document.getElementById("intro_text")
+var atlantaHeading = document.getElementById("atlanta_heading")
+var atlantaImage = document.getElementById("atlanta_image")
+var atlantaText = document.getElementById("atlanta_text")
+
+nextButton.addEventListener("click" , information);
+
+function information() {
+    
+    
+    
+}
+
+
